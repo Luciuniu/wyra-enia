38754c573a5303c7d2284a4d3f411da943c170d5
Luciuniu <luciuniu@wp.pl>
2018-10-12 16:48:41 +0200

    stworzono instrukcje

---
+++ instrukcje.c
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int lbDzieci(0);
+    if (lbDzieci > 0)
+    {
+        cout << "Masz dzieci. Brawo!" << endl;
+    }
+    else
+    {
+        cout << "Ojej, nie masz dzieci?" <<
+             endl;
+    }
+    cout << "Koniec programu" << endl;
+    return 0;
+}
